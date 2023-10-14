491feffc6b67c9e1d814d2f3d8b0e7d6abadd262
David England <david.england.de@googlemail.com>
2023-10-14 13:10:46 +0100

    Refactor: reformatting by black

--- print_stuff.py
+++ print_stuff.py
@@ -4,10 +4,15 @@ from matplotlib import pyplot as plt
 
 def do_scatterplot(i, col):
     ax = f.add_subplot(5, 2, i)
-    ax.boxplot([not_transported[col].dropna(), transported[col].dropna()], vert=False, showfliers=False)
-    ax.set_yticklabels(["Still here", "Gone...."] if i%2 != 0 else [])
+    ax.boxplot(
+        [not_transported[col].dropna(), transported[col].dropna()],
+        vert=False,
+        showfliers=False,
+    )
+    ax.set_yticklabels(["Still here", "Gone...."] if i % 2 != 0 else [])
     ax.set_title(col, loc="right", y=0.6)
     ax.set_ylim(0, 4)
+
 
 def do_bar(i, col):
     ax = f.add_subplot(5, 2, i)
@@ -17,24 +22,28 @@ def do_bar(i, col):
     for val in valid_values:
         d_still_here = not_transported[col][not_transported[col] == val].count()
         d_gone = transported[col][transported[col] == val].count()
-        ax.barh([False, True], [d_still_here, d_gone], left = [still_here, gone])
+        ax.barh([False, True], [d_still_here, d_gone], left=[still_here, gone])
         still_here += d_still_here
         gone += d_gone
-    ax.barh([False, True],
-            [len(not_transported['VIP'][not_transported['VIP'].isna() == True]),
-                len(transported['VIP'][transported['VIP'].isna() == True])],
-            left=[still_here, gone])
+    ax.barh(
+        [False, True],
+        [
+            len(not_transported["VIP"][not_transported["VIP"].isna() == True]),
+            len(transported["VIP"][transported["VIP"].isna() == True]),
+        ],
+        left=[still_here, gone],
+    )
 
     ax.set_yticks([False, True])
-    ax.set_yticklabels(["Still here", "Gone...."] if i%2 != 0 else [])
+    ax.set_yticklabels(["Still here", "Gone...."] if i % 2 != 0 else [])
     ax.set_title(col, loc="right", y=0.6)
-    ax.legend(valid_values + ["NaN"], loc = 'upper left', fontsize = 'x-small')
+    ax.legend(valid_values + ["NaN"], loc="upper left", fontsize="x-small")
     ax.set_ylim(-1, 3)
 
 
 df = pd.read_csv("data/train.csv")
-not_transported = df[df['Transported'] == False]
-transported = df[df['Transported'] == True]
+not_transported = df[df["Transported"] == False]
+transported = df[df["Transported"] == True]
 graph_type = {
     "PassengerId": "nope",
     "HomePlanet": "bar",
