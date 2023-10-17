c0f33fe6f22cacc27a347ad72964bc0c50c1ce12
David England <david.england.de@googlemail.com>
2023-10-17 10:43:14 +0100

    Plot cabin_1, cabin_3, group_2 attributes

--- groups_and_cabins_eda.py
+++ groups_and_cabins_eda.py
@@ -1,4 +1,33 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def do_bar(i, col):
+    ax = f.add_subplot(3, 2, i)
+
+    valid_values = list(df[col].dropna().sort_values().unique())
+    still_here, gone = 0, 0
+    for val in valid_values:
+        d_still_here = not_transported[col][not_transported[col] == val].count()
+        d_gone = transported[col][transported[col] == val].count()
+        ax.barh([False, True], [d_still_here, d_gone], left=[still_here, gone])
+        still_here += d_still_here
+        gone += d_gone
+    ax.barh(
+        [False, True],
+        [
+            len(not_transported["VIP"][not_transported["VIP"].isna() == True]),
+            len(transported["VIP"][transported["VIP"].isna() == True]),
+        ],
+        left=[still_here, gone],
+    )
+
+    ax.set_yticks([False, True])
+    ax.set_yticklabels(["Still here", "Gone...."] if i % 2 != 0 else [])
+    ax.set_title(col, loc="right", y=0.6)
+    ax.legend(valid_values + ["NaN"], loc="upper left", fontsize="x-small")
+    ax.set_ylim(-1, 3)
+
 
 df = pd.read_csv('data/train.csv')
 
@@ -11,4 +40,14 @@ _cabins = df['Cabin'].str.split('/', expand=True)
 _groups = df['PassengerId'].str.split('_', expand=True)
 df[['cabin_1', 'cabin_2', 'cabin_3']] = _cabins
 df[['group_1', 'group_2']] = _groups
+not_transported = df[df["Transported"] == False]
+transported = df[df["Transported"] == True]
 print(df)
+
+f = plt.figure()
+i = 0
+for col in ['cabin_1', 'cabin_3', 'group_2']:
+    i += 1
+    do_bar(i, col)
+
+f.savefig("groups_and_cabins.png")
