2803cb7a0054b8eb39cbf1fe30f5ed050388fc7d
David England <david.england.de@googlemail.com>
2023-10-17 08:18:03 +0100

    Split PassengerId column

--- groups_and_cabins_eda.py
+++ groups_and_cabins_eda.py
@@ -3,8 +3,12 @@ import pandas as pd
 df = pd.read_csv('data/train.csv')
 
 _matchCabins = df['Cabin'].str.match('[A-Z]/\\d{,4}/[A-Z]')
+_matchGroups = df['PassengerId'].str.match('\\d{4}_\\d{2}')
 print('Cabin matches A/0/A:', _matchCabins[_matchCabins == True].count(), _matchCabins[_matchCabins == False].count(), len(_matchCabins))
+print('PassengerId matches 0000_00:', _matchGroups[_matchGroups == True].count(), _matchGroups[_matchGroups == False].count(), len(_matchGroups))
 
 _cabins = df['Cabin'].str.split('/', expand=True)
+_groups = df['PassengerId'].str.split('_', expand=True)
 df[['cabin_1', 'cabin_2', 'cabin_3']] = _cabins
+df[['group_1', 'group_2']] = _groups
 print(df)
