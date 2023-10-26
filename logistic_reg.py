6cc94b49fce401cba6e174aec0532fd1790e1917
David England <david.england.de@googlemail.com>
2023-10-26 12:14:42 +0100

    Refactor: variable naming for logistic regression

--- logistic_reg.py
+++ logistic_reg.py
@@ -3,49 +3,49 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 #---- 1. LOADING
-data = pd.read_csv('data/train.csv')
+main = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
 
 #---- 2. CLEANING
-data['Transported'] = data['Transported'].astype('int32')
+main['Transported'] = main['Transported'].astype('int32')
 
-for x in [data, test]:
-    x['HomePlanet'].fillna(x['HomePlanet'].mode()[0], inplace=True)
-    x['CryoSleep'].fillna(x['CryoSleep'].mode()[0], inplace=True)
-    x['Destination'].fillna(x['Destination'].mode()[0], inplace=True)
-    x['Age'].fillna(x['Age'].mean(), inplace=True)
-    x['VIP'].fillna(x['VIP'].mode()[0], inplace=True)
-    x['RoomService'].fillna(x['RoomService'].mean(), inplace=True)
-    x['FoodCourt'].fillna(x['FoodCourt'].mean(), inplace=True)
-    x['ShoppingMall'].fillna(x['ShoppingMall'].mean(), inplace=True)
-    x['Spa'].fillna(x['Spa'].mean(), inplace=True)
-    x['VRDeck'].fillna(x['VRDeck'].mean(), inplace=True)
+for df in [main, test]:
+    df['HomePlanet'].fillna(df['HomePlanet'].mode()[0], inplace=True)
+    df['CryoSleep'].fillna(df['CryoSleep'].mode()[0], inplace=True)
+    df['Destination'].fillna(df['Destination'].mode()[0], inplace=True)
+    df['Age'].fillna(df['Age'].mean(), inplace=True)
+    df['VIP'].fillna(df['VIP'].mode()[0], inplace=True)
+    df['RoomService'].fillna(df['RoomService'].mean(), inplace=True)
+    df['FoodCourt'].fillna(df['FoodCourt'].mean(), inplace=True)
+    df['ShoppingMall'].fillna(df['ShoppingMall'].mean(), inplace=True)
+    df['Spa'].fillna(df['Spa'].mean(), inplace=True)
+    df['VRDeck'].fillna(df['VRDeck'].mean(), inplace=True)
 
-data = pd.concat([data, pd.DataFrame({"HomePlanet_Earth": data['HomePlanet'] == 'Earth',
-                                    "HomePlanet_Europa": data['HomePlanet'] == 'Europa'})], axis=1)
-data = pd.concat([data, pd.DataFrame({"Destination_TRAPPIST": data['Destination'] == 'TRAPPIST-1e',
-                                    "Destination_PSO": data['Destination'] == 'PSO J318.5-22'})], axis=1)
+main = pd.concat([main, pd.DataFrame({"HomePlanet_Earth": main['HomePlanet'] == 'Earth',
+                                    "HomePlanet_Europa": main['HomePlanet'] == 'Europa'})], axis=1)
+main = pd.concat([main, pd.DataFrame({"Destination_TRAPPIST": main['Destination'] == 'TRAPPIST-1e',
+                                    "Destination_PSO": main['Destination'] == 'PSO J318.5-22'})], axis=1)
 test = pd.concat([test, pd.DataFrame({"HomePlanet_Earth": test['HomePlanet'] == 'Earth',
                                     "HomePlanet_Europa": test['HomePlanet'] == 'Europa'})], axis=1)
 test = pd.concat([test, pd.DataFrame({"Destination_TRAPPIST": test['Destination'] == 'TRAPPIST-1e',
                                     "Destination_PSO": test['Destination'] == 'PSO J318.5-22'})], axis=1)
 
 #---- 3. TRAIN-TEST SPLIT
-data_train = data.sample(frac=.8, random_state=400)
-data_test = data.drop(data_train.index.tolist())
+main_train = main.sample(frac=.8, random_state=400)
+main_test = main.drop(main_train.index.tolist())
 
 #---- 4. FITTING
 model = smf.logit(
     'Transported ~ HomePlanet_Earth + HomePlanet_Europa + CryoSleep + Destination_TRAPPIST + Destination_PSO + Age' \
     ' + RoomService + FoodCourt + ShoppingMall + Spa + VRDeck' \
-    , data_train)
+    , main_train)
 fit_results = model.fit()
 print(fit_results.summary())
 print(fit_results.pred_table())
 
 #---- 5. PREDICTING
-train_predicts = fit_results.predict(data_test.drop(columns='Transported'))
-train_results = pd.DataFrame({'actual': data_test['Transported'], 'predicted': train_predicts})
+train_predicts = fit_results.predict(main_test.drop(columns='Transported'))
+train_results = pd.DataFrame({'actual': main_test['Transported'], 'predicted': train_predicts})
 train_results['predicted'] = (train_results['predicted'] >= .5).astype('int32')
 
 print('\nPrediction accuracy for test set:')
