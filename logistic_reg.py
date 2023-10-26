f7c37fa6b14567a3cdb949070fe3190b46595cb6
David England <david.england.de@googlemail.com>
2023-10-26 10:52:32 +0100

    Third imputation attempt for logistic regression
    Including backwards elimination (VIP)

--- logistic_reg.py
+++ logistic_reg.py
@@ -7,18 +7,22 @@ data = pd.read_csv('data/train.csv')
 
 #---- 2. CLEANING
 data['Transported'] = data['Transported'].astype('int32')
-data["IsNull_" + data.columns] = data.isna()
+
+data['HomePlanet'].fillna(data['HomePlanet'].mode()[0], inplace=True)
+data['CryoSleep'].fillna(data['CryoSleep'].mode()[0], inplace=True)
+data['Destination'].fillna(data['Destination'].mode()[0], inplace=True)
+data['Age'].fillna(data['Age'].mean(), inplace=True)
+data['VIP'].fillna(data['VIP'].mode()[0], inplace=True)
+data['RoomService'].fillna(data['RoomService'].mean(), inplace=True)
+data['FoodCourt'].fillna(data['FoodCourt'].mean(), inplace=True)
+data['ShoppingMall'].fillna(data['ShoppingMall'].mean(), inplace=True)
+data['Spa'].fillna(data['Spa'].mean(), inplace=True)
+data['VRDeck'].fillna(data['VRDeck'].mean(), inplace=True)
 
 data = pd.concat([data, pd.DataFrame({"HomePlanet_Earth": data['HomePlanet'] == 'Earth',
-                                      "HomePlanet_Europa": data['HomePlanet'] == 'Europa',
-                                      "HomePlanet_Mars": data['HomePlanet'] == 'Mars'})], axis=1)
+                                      "HomePlanet_Europa": data['HomePlanet'] == 'Europa'})], axis=1)
 data = pd.concat([data, pd.DataFrame({"Destination_TRAPPIST": data['Destination'] == 'TRAPPIST-1e',
-                                      "Destination_PSO": data['Destination'] == 'PSO J318.5-22',
-                                      "Destination_Cancri": data['Destination'] == '55 Cancri e'})], axis=1)
-
-data[['CryoSleep', 'VIP']] = data[['CryoSleep', 'VIP']].fillna(False)
-data['Age'].fillna(0., inplace=True)
-data[['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']] = data[['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']].fillna(0.)
+                                      "Destination_PSO": data['Destination'] == 'PSO J318.5-22'})], axis=1)
 
 #---- 3. TRAIN-TEST SPLIT
 data_train = data.sample(frac=.8, random_state=400)
@@ -26,8 +30,8 @@ data_test = data.drop(data_train.index.tolist())
 
 #---- 4. FITTING
 model = smf.logit(
-    'Transported ~ HomePlanet_Earth + HomePlanet_Europa + CryoSleep + IsNull_CryoSleep + Destination_TRAPPIST + Destination_PSO + Age' \
-    ' + RoomService + IsNull_RoomService + FoodCourt + ShoppingMall + Spa + VRDeck' \
+    'Transported ~ HomePlanet_Earth + HomePlanet_Europa + CryoSleep + Destination_TRAPPIST + Destination_PSO + Age' \
+    ' + RoomService + FoodCourt + ShoppingMall + Spa + VRDeck' \
     , data_train)
 fit_results = model.fit()
 print(fit_results.summary())
